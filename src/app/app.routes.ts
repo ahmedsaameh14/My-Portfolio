d4f91a41a9cf51d66f4dc6e30cfc663a598353dc
Ahmed_Sameh <ahmedsaameh14@gmail.com>
2025-09-10 19:03:07 +0300

    Add Routes - Add app Tags in Main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './sections/main/main.component';
+import { LayoutComponent } from './sections/layout/layout.component';
+import { AboutComponent } from './sections/about/about.component';
+import { SkillsComponent } from './sections/skills/skills.component';
+import { EducationComponent } from './sections/education/education.component';
+import { ExperienceComponent } from './sections/experience/experience.component';
+import { ProjectsComponent } from './sections/projects/projects.component';
+import { ContactComponent } from './sections/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'' , component:LayoutComponent,
+        children: [
+            {path:'' , component: MainComponent},
+            {path:'about' , component: AboutComponent},
+            {path:'skills' , component: SkillsComponent},
+            {path:'education' , component: EducationComponent},
+            {path:'experience' , component: ExperienceComponent},
+            {path:'projects' , component: ProjectsComponent},
+            {path:'contact' , component: ContactComponent},
+            {path: '', redirectTo: '', pathMatch: 'full' },
+        ]
+    }
+
+];
--- src/app/sections/main/main.component.ts
+++ src/app/sections/main/main.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from "../home/home.component";
+import { AboutComponent } from "../about/about.component";
+import { SkillsComponent } from "../skills/skills.component";
+import { EducationComponent } from "../education/education.component";
+import { ExperienceComponent } from "../experience/experience.component";
+import { ProjectsComponent } from "../projects/projects.component";
+import { ContactComponent } from "../contact/contact.component";
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [HomeComponent, AboutComponent, SkillsComponent, EducationComponent, ExperienceComponent, ProjectsComponent, ContactComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
