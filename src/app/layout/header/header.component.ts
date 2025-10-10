8fc428f20d120bde93b70ed99df5eb554d800106
Ahmed_Sameh <ahmedsaameh14@gmail.com>
2025-10-11 00:52:54 +0300

    Edit in Navbar Toggle

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component } from '@angular/core';
+var bootstrap:any;
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
 
+  ngAfterViewInit() {
+    // Select the navbar collapse element
+    const navbarCollapse = document.getElementById('portfolioNavbar');
+
+    // Get all nav links
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    // Loop through each link and close collapse on click
+    navLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        const bsCollapse = bootstrap.Collapse.getInstance(navbarCollapse);
+        if (bsCollapse) {
+          bsCollapse.hide();
+        }
+      });
+    });
+  }
 }
