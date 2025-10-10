5f2b19bae2e61a736dee8cbaa50a62aca731dfdc
Ahmed_Sameh <ahmedsaameh14@gmail.com>
2025-10-11 01:30:11 +0300

    Edit in Sizing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { MainComponent } from './sections/main/main.component';
 import { LayoutComponent } from './sections/layout/layout.component';
 import { AboutComponent } from './sections/about/about.component';
 import { SkillsComponent } from './sections/skills/skills.component';
-import { EducationComponent } from './sections/education/education.component';
+// import { EducationComponent } from './sections/education/education.component';
 import { ExperienceComponent } from './sections/experience/experience.component';
 import { ProjectsComponent } from './sections/projects/projects.component';
 import { ContactComponent } from './sections/contact/contact.component';
--- src/app/sections/main/main.component.ts
+++ src/app/sections/main/main.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { HomeComponent } from "../home/home.component";
 import { AboutComponent } from "../about/about.component";
 import { SkillsComponent } from "../skills/skills.component";
-import { EducationComponent } from "../education/education.component";
+// import { EducationComponent } from "../education/education.component";
 import { ExperienceComponent } from "../experience/experience.component";
 import { ProjectsComponent } from "../projects/projects.component";
 import { ContactComponent } from "../contact/contact.component";
 
 @Component({
   selector: 'app-main',
-  imports: [HomeComponent, AboutComponent, SkillsComponent, EducationComponent, ExperienceComponent, ProjectsComponent, ContactComponent],
+  imports: [HomeComponent, AboutComponent, SkillsComponent, ExperienceComponent, ProjectsComponent, ContactComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
