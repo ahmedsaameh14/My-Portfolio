566f46bd0840fd34c639473856a8398d80b4a2ee
Ahmed_Sameh <ahmedsaameh14@gmail.com>
2025-11-27 18:23:54 +0200

    Improve Accessibility

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component } from '@angular/core';
-var bootstrap:any;
+declare var bootstrap:any;
 @Component({
   selector: 'app-header',
   imports: [],
@@ -8,21 +8,10 @@ var bootstrap:any;
 })
 export class HeaderComponent implements AfterViewInit {
 
-  ngAfterViewInit() {
-    // Select the navbar collapse element
-    const navbarCollapse = document.getElementById('portfolioNavbar');
-
-    // Get all nav links
-    const navLinks = document.querySelectorAll('.nav-link');
-
-    // Loop through each link and close collapse on click
-    navLinks.forEach(link => {
-      link.addEventListener('click', () => {
-        const bsCollapse = bootstrap.Collapse.getInstance(navbarCollapse);
-        if (bsCollapse) {
-          bsCollapse.hide();
-        }
-      });
-    });
+  ngAfterViewInit(): void {
+    const navElement = document.getElementById('portfolioNavbar');
+    if (navElement) {
+      new bootstrap.Collapse(navElement, { toggle: false });
+    }
   }
 }
